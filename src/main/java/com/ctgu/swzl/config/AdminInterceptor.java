b8f74a44e134d91d500b6996cc63f3bf6e436c87
surkuang <surkuang@qq.com>
2020-06-25 21:11:29 +0800

    删除多余代码

--- src/main/java/com/ctgu/swzl/config/AdminInterceptor.java
+++ src/main/java/com/ctgu/swzl/config/AdminInterceptor.java
@@ -1,18 +1,11 @@
 package com.ctgu.swzl.config;
 
-import com.ctgu.swzl.domain.User;
-import com.ctgu.swzl.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class AdminInterceptor implements HandlerInterceptor {
-
-    @Autowired
-    private UserService userService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
